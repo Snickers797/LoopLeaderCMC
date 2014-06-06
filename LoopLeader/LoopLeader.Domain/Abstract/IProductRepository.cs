e1deb6c4812c4b04a6fa39ab7cc46dbb2c764a1c
Marti Garcia <marta.garcia.a@gmail.com>
2014-06-06 11:29:19 -0700

    Initial Build

---
+++ LoopLeader/LoopLeader.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LoopLeader.Domain.Entities;
+
+namespace LoopLeader.Domain.Abstract
+{
+    public interface IProduct
+    {
+        void AddProduct(Product product);
+        IQueryable<Product> Products { get; }
+
+        Product GetProductByProductID(int ProductID);
+        Product GetProductByProductName(string ProductName);
+        void SaveProduct(Product product);
+        Product DeleteProduct(int ProductID);
+
+
+    }
+
+
+}
---
+++ LoopLeader/LoopLeader/Models/ProductsListViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LoopLeader.Domain.Abstract;
+using LoopLeader.Domain.Entities;
+
+namespace LoopLeader.Models
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
+
+        public int Quantity { get; set; }
+        public decimal Total { get; set; }
+
+    }
+}
